056c4d4cd8f9a047f0bd3992188282a374c74200
vgarshin <vgarshin@yandex.ru>
2023-05-29 13:09:06 +0000

    mlflow demo added

---
+++ manutils/start-mlflow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Home directory for MLflow
+MLFLOW_PATH=/home/jovyan/${JUPYTERHUB_USER}_mlflow
+
+# Install MLflow
+pip install mlflow
+
+# Start MLflow
+cd ~ && mlflow server --host 0.0.0.0 --port 50000 \
+  --backend-store-uri file://${MLFLOW_PATH} \
+  --default-artifact-root file://${MLFLOW_PATH} \
+  &>/dev/null & disown;
+
