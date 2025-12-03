45caaf130eb4845e7b239720ef87291174650279
Vasiliy Garshin <vgarshin@yandex.ru>
2025-12-03 13:57:34 +0000

    mlflow ver

--- manutils/start-mlflow.sh
+++ manutils/start-mlflow.sh
@@ -4,7 +4,7 @@
 MLFLOW_PATH=/home/jovyan/${JUPYTERHUB_USER}_mlflow
 
 # Install MLflow
-pip install mlflow
+pip install mlflow==2.19.0
 
 # Start MLflow
 cd ~ && mlflow server --host 0.0.0.0 --port 50000 \
